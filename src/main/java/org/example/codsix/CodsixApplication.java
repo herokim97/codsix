96dc0f56bc4427e8dc7ce7875034eb0394023bb8
heroKim997 <ghkufud6545@gmail.com>
2024-12-23 21:47:28 +0900

    git init

---
+++ src/main/java/org/example/codsix/CodsixApplication.java
@@ -0,0 +1,13 @@
+package org.example.codsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodsixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodsixApplication.class, args);
+    }
+
+}
